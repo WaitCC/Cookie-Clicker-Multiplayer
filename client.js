f6dcd2c2237693becafc33cb747084da6e42f51a
Waitdev_ <wbusites@gmail.com>
2020-05-02 00:20:51 +0930

    i'm a dumbass

--- client.js
+++ client.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Cookie Clicker Multiplayer
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Adds multiplayer functionality to Cookie Clicker
 // @author       Louis Rust
 // @match        https://orteil.dashnet.org/cookieclicker/
